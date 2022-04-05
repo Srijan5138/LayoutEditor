2f0dc6a6f7271151ed315250d9ec26ab4ba2f554
unknown <acharyasrijan360@gmail.com>
2022-04-05 14:19:29 +0545

    challengecompleted

--- app/src/main/java/com/example/layouteditor/MainActivity.java
+++ app/src/main/java/com/example/layouteditor/MainActivity.java
@@ -19,26 +19,12 @@ public class MainActivity extends AppCompatActivity {
         mShowCount = (TextView) findViewById(R.id.show_count);
     }
 
-    /*
-     * Shows a Toast when the TOAST button is clicked.
-     *
-     * @param view The view that triggered this onClick handler.
-     *             Since a toast always shows on the top,
-     *             the passed in view is not used.
-     */
     public void showToast(View view) {
         Toast toast = Toast.makeText(this, R.string.toast_message,
                 Toast.LENGTH_SHORT);
         toast.show();
     }
 
-    /*
-     * Increments the number in the TextView when the COUNT button is clicked.
-     *
-     * @param view The view that triggered this onClick handler.
-     *             Since the count always appears in the TextView,
-     *             the passed in view is not used.
-     */
     public void countUp(View view) {
         mCount++;
         if (mShowCount != null)
